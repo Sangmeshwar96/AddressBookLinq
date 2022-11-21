cca1199f76d5b28039bf40d30d43f597eb61d794
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 20:32:48 +0530

    Address Book System Using Linq

---
+++ AddressBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Linq
+{
+    class AddressBook
+    {
+        public void AddToDataTable()
+        {
+
+            DataTable table = new DataTable();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address book linq problem!");
+            AddressBook addressBook = new AddressBook();
+            Console.ReadLine();
+
+        }
+        
+    }
+
+    
+}
