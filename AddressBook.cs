ebb83f1bdcd2e2196d5c815a0f308e03d5c51c48
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 20:38:38 +0530

    Adding Attributes

--- AddressBook.cs
+++ AddressBook.cs
@@ -9,10 +9,18 @@ namespace AddressBook_Linq
 {
     class AddressBook
     {
-        public void AddToDataTable()
+        DataTable table = new DataTable("AddressBook");
+        public AddressBook()
         {
 
-            DataTable table = new DataTable();
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(string));
+            table.Columns.Add("PhoneNumber", typeof(string));
+            table.Columns.Add("Email", typeof(string));
         }
     }
 }
