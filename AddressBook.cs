a96e466d798f3b14ddb20cd720994c849ea87b65
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-11-21 21:28:32 +0530

    Sorting Contact Alphabetically For Given City

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,19 +1,19 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace AddressBook_Linq
+namespace AddressBookLINQ
 {
-    class AddressBook
+    public class AddressBookDataTable
     {
+
         DataTable table = new DataTable("AddressBook");
-        public AddressBook()
+
+
+        public AddressBookDataTable()
         {
-    
             table.Columns.Add("FirstName", typeof(string));
             table.Columns.Add("LastName", typeof(string));
             table.Columns.Add("Address", typeof(string));
@@ -22,141 +22,70 @@ namespace AddressBook_Linq
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            table.Rows.Add("Mukhesh", "Attuluri", "8-47", "Ppm", "Ap", "535501", "8978496720", "mkh.com");
+            table.Rows.Add("Ram", "Bantu", "Sun nagar", "Vizag", "Ap", "546489", "8570456737", "ram.com");
+            table.Rows.Add("Ravi", "Kumar", "Rain colony", "Hyd", "Telangana", "546362", "9878678593", "ravi.com");
+            table.Rows.Add("Srinu", "Rao", "WhiteField", "Banglore", "Karnataka", "125445", "7206326427", "srinu.com");
         }
-     
-        public void InsertContactToTable()
+        public void GetAllContacts()
         {
-           
-            table.Rows.Add("Shreya", "Malviya", "Howrah", "Nagpur", "MH", 75866, "8596748585", "shreya@gmail.com");
-            table.Rows.Add("prajakta", "Nayak", "Durgapur", "A zone", "West Bengal", 14526, "8596748585", "prajakta@gmail.com");
-            table.Rows.Add("Tanmay", "Agarwal", "Kolkata", "NewTown", "West Bengal", 78596, "8596748585", "tanmay@gmail.com");
-            table.Rows.Add("ekta", "Nath", "Patna", "Patna City", "Bihar", 125896, "8596748585", "ekta@gmail.com");
-            Console.WriteLine("Contact details added successfully!\n Select 2 for view contact\n");
-
-
+            foreach (DataRow dr in table.AsEnumerable())
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("FirstName"));
+                Console.WriteLine("lastName:- " + dr.Field<string>("LastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("Address"));
+                Console.WriteLine("City:- " + dr.Field<string>("City"));
+                Console.WriteLine("State:- " + dr.Field<string>("State"));
+                Console.WriteLine("zip:- " + dr.Field<string>("Zip"));
+                Console.WriteLine("phoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("eMail:- " + dr.Field<string>("Email"));
+            }
 
         }
 
-        public void DisplayDetails()
+
+        public void UpdateContact(string firstName, string lastName, string columnName, string newValue)
         {
-
-            foreach (var table in table.AsEnumerable())
-            {
-                
-                Console.WriteLine("\nFirstName:-" + table.Field<string>("FirstName"));
-                Console.WriteLine("LastName:-" + table.Field<string>("LastName"));
-                Console.WriteLine("Address:-" + table.Field<string>("Address"));
-                Console.WriteLine("City:-" + table.Field<string>("City"));
-                Console.WriteLine("State:-" + table.Field<string>("State"));
-                Console.WriteLine("ZipCode:-" + table.Field<string>("Zip"));
-                Console.WriteLine("PhoneNumber:-" + table.Field<string>("PhoneNumber"));
-                Console.WriteLine("Email:-" + table.Field<string>("Email"));
-            }
+            DataRow updateContact = table.Select("FirstName = '" + firstName + "' and LastName = '" + lastName + "'").FirstOrDefault();
+            updateContact[columnName] = newValue;
+            Console.WriteLine("Updated Contact");
         }
-
-        public void EditExistingContact()
+        public void DeleteContact(string firstName, string lastName)
         {
-            try
-            {
-                
-                string editName = "prajakta";
-
-                var updateData = table.AsEnumerable().Where(x => x.Field<string>("FirstName").Equals(editName)).FirstOrDefault();
-                if (updateData != null)
-                {
-                    
-                    updateData.SetField("PhoneNumber", "895478520");
-                    updateData.SetField("City", "Pune");
-                    Console.WriteLine("\n PhoneNumber and ity of {0} updated successfully!", editName);
-                    DisplayDetails();
-                }
-                else
-                {
-                    Console.WriteLine("There is no such record in the Address Book!");
-                }
-            }
-            
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            DataRow deleteContact = table.Select("FirstName = '" + firstName + "' and LastName = '" + lastName + "'").FirstOrDefault();
+            table.Rows.Remove(deleteContact);
         }
-        public void DeleteContact(string firstName)
+        public void RetrieveByCityOrState(string city, string state)
         {
-            try
+            ArrayList list = new ArrayList();
+            var contact = from c in table.AsEnumerable()
+                          where c.Field<string>("City") == city || c.Field<string>("State") == state
+                          select c;
+            foreach (DataRow dr in contact)
             {
-
-                var rowDelete = table.AsEnumerable().Where(a => a.Field<string>("FirstName").Equals(firstName)).FirstOrDefault();
-                if (rowDelete != null)
-                {
-                    
-
-                    rowDelete.Delete();
-                    Console.WriteLine("\nContact with name '{0}' deleted successfully!", firstName);
-                    DisplayDetails();
-                }
-                else
-                {
-                    Console.WriteLine("There is no such data");
-                }
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("FirstName"));
+                Console.WriteLine("lastName:- " + dr.Field<string>("LastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("Address"));
+                Console.WriteLine("City:- " + dr.Field<string>("City"));
+                Console.WriteLine("State:- " + dr.Field<string>("State"));
+                Console.WriteLine("zip:- " + dr.Field<string>("Zip"));
+                Console.WriteLine("phoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("eMail:- " + dr.Field<string>("Email"));
             }
-            
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
         }
-        public void retrievePersonByUsingCity(Contact contact)
+        public void CountByCityOrState(string city, string state)
         {
- 
-            var selectdData = table.AsEnumerable().Where(a => a.Field<string>("City").Equals(contact.City)).FirstOrDefault();
+            var contact = from c in table.AsEnumerable()
+                          where c.Field<string>("City") == city && c.Field<string>("State") == state
+                          select c;
 
-            foreach (var table in table.AsEnumerable())
-            {
-                Console.WriteLine("\nFirstName:-" + table.Field<string>("FirstName"));
-                Console.WriteLine("LastName:-" + table.Field<string>("LastName"));
-                Console.WriteLine("Address:-" + table.Field<string>("Address"));
-                Console.WriteLine("City:-" + table.Field<string>("City"));
-                Console.WriteLine("State:-" + table.Field<string>("State"));
-                Console.WriteLine("ZipCode:-" + table.Field<int>("ZipCode"));
-                Console.WriteLine("PhoneNumber:-" + table.Field<long>("PhoneNumber"));
-                Console.WriteLine("Email:-" + table.Field<string>("Email"));
-            }
-
+            Console.WriteLine("Count of contacts in City:{0} and  State:{1} is {2}", city, state, contact.Count());
         }
-        public void retrievePersonByUsingState(Contact contact)
-        {
- 
-            var selectdData = table.AsEnumerable().Where(a => a.Field<string>("State").Equals(contact.State)).FirstOrDefault();
 
-            foreach (var table in table.AsEnumerable())
-            {
-                Console.WriteLine("\nFirstName:-" + table.Field<string>("FirstName"));
-                Console.WriteLine("LastName:-" + table.Field<string>("LastName"));
-                Console.WriteLine("Address:-" + table.Field<string>("Address"));
-                Console.WriteLine("City:-" + table.Field<string>("City"));
-                Console.WriteLine("State:-" + table.Field<string>("State"));
-                Console.WriteLine("ZipCode:-" + table.Field<int>("ZipCode"));
-                Console.WriteLine("PhoneNumber:-" + table.Field<long>("PhoneNumber"));
-                Console.WriteLine("Email:-" + table.Field<string>("Email"));
-            }
 
-        }
-        public void sortContactAlphabeticallyForGivenCity(Contact contact)
-        {
-            
-            var records = table.AsEnumerable().Where(x => x.Field<string>("City") == contact.City).OrderBy(x => x.Field<string>("FirstName")).ThenBy(x => x.Field<string>("LastName"));
-            foreach (var table in records)
-            {
-                Console.WriteLine("\nFirstName:-" + table.Field<string>("FirstName"));
-                Console.WriteLine("LastName:-" + table.Field<string>("LastName"));
-                Console.WriteLine("Address:-" + table.Field<string>("Address"));
-                Console.WriteLine("City:-" + table.Field<string>("City"));
-                Console.WriteLine("State:-" + table.Field<string>("State"));
-                Console.WriteLine("ZipCode:-" + table.Field<string>("Zip"));
-                Console.WriteLine("PhoneNumber:-" + table.Field<string>("PhoneNumber"));
-                Console.WriteLine("Email:-" + table.Field<string>("Email"));
-            }
-        }
     }
 }
